4accd3814521e067e4eda2f02c795271b50be2cc
lucyq <lucyzqin@gmail.com>
2015-01-02 19:36:58 -0500

    rendering markers

--- public/js/mapView.js
+++ public/js/mapView.js
@@ -2,6 +2,8 @@
 var BLS;
 var BLS_lat = 42.3380;
 var BLS_long = -71.1020;
+
+var geocoder;
 var map;
 
 
@@ -12,26 +14,52 @@ var mapOptions = {
 };
 
 function init() {
-	
+
+	geocoder = new google.maps.Geocoder();
+	BLS = new google.maps.LatLng(BLS_lat, BLS_long);
 	map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
 	var request = new XMLHttpRequest();
-		var data_link = "http://localhost/location_data"; // || "tiu-proj.herokuapp.com/location_data";
+		var data_link = "http://localhost:5000/location_data"; // || "tiu-proj.herokuapp.com/location_data";
 		request.open("GET", data_link, true);
 		request.send();
 		request.onreadystatechange = function() {
 			if (request.readyState==4 && request.status==200) {
-				BLS = new google.maps.LatLng(BLS_lat, BLS_long);
+				
+				data = JSON.parse(request.responseText);
+				
+				codeAddress();
 				renderMap();
-				data = JSON.parse(request.responseText);	
+				// console.log(data);	
 			}
 	};
 		map.panTo(BLS);
 
 };
 
+function codeAddress() {
+
+	var count = 0;
+	for (k in data) {
+		var address = data[k]["location_address"];
+		geocoder.geocode({'address': address}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				var pos = results[0].geometry.location;
+				console.log("Position: " + pos);
+				var marker = new google.maps.Marker({
+					map: map,
+					position: pos
+				});
+			}
+		});
+		count++;
+	}
+}
+
 
 function renderMap() {
+//	console.log(data[0]["location_address"]);
+	// data[i]["location_address"]
 //	var locations = data
 	// create map
 	// update map and go there!
